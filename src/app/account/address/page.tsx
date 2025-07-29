39ec85ac00bba92b15661d9096db21211717674a
Quinten <quintenclaes7@gmail.com>
2025-07-29 16:44:34 +0200

    feat: add address management options with Edit, Delete links and Add Address button in Account Address page

--- src/app/account/address/page.tsx
+++ src/app/account/address/page.tsx
@@ -5,6 +5,8 @@ import { useEffect, useState } from "react";
 import { User as UserType } from "@/types/user";
 import { redirect } from "next/navigation";
 import Container from "@/components/Container";
+import Button from "@/components/Button";
+import AccountNavLink from "@/components/accountNav/AccountNavLink";
 
 const Address = () => {
   const [user, setUser] = useState<UserType | null>(null);
@@ -25,9 +27,23 @@ const Address = () => {
     <div>
       <h1>Address</h1>
       <Container className="flex items-center justify-center flex-col max-w-[400px] gap-4 p-8 border border-gray-200">
-        <p className="text-lg font-medium">
-          {user?.firstName} {user?.lastName}
-        </p>
+        <div className="flex flex-col gap-4">
+          <p className="text-lg font-medium">
+            {user?.firstName} {user?.lastName}
+          </p>
+          <div className="flex gap-2 text-sm">
+            <AccountNavLink href="/account/address/edit" isActive={true}>
+              Edit
+            </AccountNavLink>
+            <AccountNavLink href="/account/address/delete" isActive={true}>
+              Delete
+            </AccountNavLink>
+          </div>
+        </div>
+      </Container>
+
+      <Container className="flex items-center justify-center">
+        <Button>Add Address</Button>
       </Container>
     </div>
   );
